c5fbb1690413af49b25f023c78ef7253d0887339
Ubuntu <ubuntu@ip-172-31-29-164.ec2.internal>
2025-03-29 10:07:45 +0000

    my maven build and test

--- src/test/com/fortune/myapp/AppTest.java
+++ src/test/com/fortune/myapp/AppTest.java
@@ -21,6 +21,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello test World!", app.getMessage());
     }
 }
